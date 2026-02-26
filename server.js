f6287c920d305adc966aa9ffc561110dbf82bf2f
Aigamer109 <hall.aiden2012@gmail.com>
2026-02-26 00:22:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,50 +1,55 @@
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const path = require("path");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 // Serve static files from 'public'
 app.use(express.static(path.join(__dirname, "public")));
 
 // Serve index.html
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 // Store current state of all vehicles
 let vehicles = {}; // { playerId: [blocks] }
 
-io.on("connection", socket => {
-    console.log("Player connected:", socket.id);
+io.on("connection", (socket) => {
+  console.log("Player connected:", socket.id);
 
-    // Send existing blocks to new player
-    if (vehicles[socket.id]) {
-        vehicles[socket.id].forEach(block => {
-            socket.emit("updateBlock", { ...block, playerId: socket.id });
-        });
-    }
-
-    // When a player places a block
-    socket.on("placeBlock", data => {
-        // Store the block for this player
-        if (!vehicles[socket.id]) vehicles[socket.id] = [];
-        vehicles[socket.id].push(data);
-
-        // Broadcast to all other players
-        socket.broadcast.emit("updateBlock", { ...data, playerId: socket.id });
+  // Send existing blocks to new player
+  if (vehicles[socket.id]) {
+    vehicles[socket.id].forEach((block) => {
+      socket.emit("updateBlock", { ...block, playerId: socket.id });
     });
+  }
 
-    // Player disconnect
-    socket.on("disconnect", () => {
-        console.log("Player disconnected:", socket.id);
-        delete vehicles[socket.id];
-    });
+  // When a player places a block
+  socket.on("placeBlock", (data) => {
+    if (!vehicles[socket.id]) vehicles[socket.id] = [];
+    vehicles[socket.id].push(data);
+
+    // Broadcast to all other players
+    socket.broadcast.emit("updateBlock", { ...data, playerId: socket.id });
+  });
+
+  // Player disconnect
+  socket.on("disconnect", () => {
+    console.log("Player disconnected:", socket.id);
+    delete vehicles[socket.id];
+  });
 });
 
 // Start server
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
