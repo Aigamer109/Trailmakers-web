8a83c0ee85fba1d54568f66f967172fb3b8e7a58
Aigamer109 <hall.aiden2012@gmail.com>
2026-02-25 22:46:32 -0500

    4

---
+++ Public/Constraints.js
@@ -0,0 +1,9 @@
+export const BLOCK_SIZES = {
+    "1x2":[1,1,2],
+    "2x4":[2,1,4],
+    "4x4":[4,1,4],
+    "seat":[1,1,1],
+    "engine":[1,1,1],
+    "wheel":[1,1,1],
+    "steering":[1,1,1]
+};
