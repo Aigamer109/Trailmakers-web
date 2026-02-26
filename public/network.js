87fd5bb40593e15889bdf1844fba72db129d3982
Aigamer109 <hall.aiden2012@gmail.com>
2026-02-26 00:14:50 -0500

    Update network.js

--- public/network.js
+++ public/network.js
@@ -1,55 +1,30 @@
+import io from "https://cdn.socket.io/4.7.2/socket.io.esm.min.js";
+
 export class Network {
-    constructor(vehicle, player){
-        this.socket = io();
+    constructor(vehicle) {
         this.vehicle = vehicle;
-        this.player = player;
-        this.others = {};
+        this.socket = io();
 
-        this.socket.on("init", players=>{
-            for(let id in players){
-                if(id !== this.socket.id){
-                    this.spawnOther(players[id]);
-                }
-            }
+        // When another player joins or updates their vehicle
+        this.socket.on("updateBlock", data => {
+            if (data.playerId === this.socket.id) return;
+            this.vehicle.updateRemoteBlock(data);
         });
 
-        this.socket.on("playerJoined", data=>{
-            this.spawnOther(data);
-        });
-
-        this.socket.on("playerState", data=>{
-            if(this.others[data.id]){
-                this.others[data.id].position.set(
-                    data.position.x,
-                    data.position.y,
-                    data.position.z
-                );
-            }
-        });
-
-        this.socket.on("removePlayer", id=>{
-            if(this.others[id]){
-                this.vehicle.scene.remove(this.others[id]);
-                delete this.others[id];
-            }
+        this.socket.on("playerJoined", data => {
+            console.log("Player joined:", data.id);
         });
     }
 
-    spawnOther(data){
-        const geo = new THREE.BoxGeometry(1,2,1);
-        const mat = new THREE.MeshStandardMaterial({color:0xff0000});
-        const mesh = new THREE.Mesh(geo,mat);
-        this.vehicle.scene.add(mesh);
-        this.others[data.id] = mesh;
+    sendBlock(type, pos, rotation) {
+        this.socket.emit("placeBlock", {
+            type,
+            pos: { x: pos.x, y: pos.y, z: pos.z },
+            rotation
+        });
     }
 
-    update(){
-        this.socket.emit("stateUpdate", {
-            id: this.socket.id,
-            position: this.player.body.position,
-            rotation: this.vehicle.body.quaternion,
-            build: this.vehicle.serialize(),
-            health: this.vehicle.health
-        });
+    update() {
+        // Add any regular position/rotation updates here if needed
     }
 }
